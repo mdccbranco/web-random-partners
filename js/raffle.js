94bccc75123186d8ddc3994e0d97f37b1c746cb5
Mateus Felix <mateus.cfoliveira@gmail.com>
2019-01-10 17:37:55 -0200

    web_sao_jan19 updated

--- js/raffle.js
+++ js/raffle.js
@@ -1,65 +1,62 @@
 class Raffle {
   constructor() {
-    this.students = _.shuffle([{
-      name: 'Andre',
-      img: './img/Andre.jpg'
-    },
-    {
-      name: 'Carol',
-      img: './img/Carol.jpg'
-    },
-    {
-      name: 'Daniel',
-      img: './img/Daniel.jpg'
-    },
-    {
-      name: 'Elaine',
-      img: './img/Elaine.jpg'
-    },
-    {
-      name: 'Gabriel',
-      img: './img/Gabriel.jpg'
-    },
-    {
-      name: 'Guilherme',
-      img: './img/Gui.jpg'
-    },
-    {
-      name: 'Ismael',
-      img: './img/Ismael.jpg'
-    },
-    {
-      name: 'João',
-      img: './img/Joao.jpg'
-    },
-    {
-      name: 'Mohsan',
-      img: './img/Mohsan.jpg'
-    },
-    {
-      name: 'Radisol',
-      img: './img/Radisol.jpg'
-    },
-    {
-      name: 'Romulo',
-      img: './img/Romulo.jpg'
-    },
-    {
-      name: 'Ronaldo',
-      img: './img/Ronaldo.jpg'
-    },
-    {
-      name: 'Sarah',
-      img: './img/Sarah.jpg'
-    }
+    this.students = _.shuffle([
+      {
+        name: 'Bruno',
+        img: './img/Bruno.jpg'
+      },
+      {
+        name: 'Cristiana',
+        img: './img/Cristiana.jpg'
+      },
+      {
+        name: 'Guilherme',
+        img: './img/Guilherme.jpg'
+      },
+      {
+        name: 'Henrique',
+        img: './img/Henrique.jpg'
+      },
+      {
+        name: 'Ilara',
+        img: './img/Ilara.jpg'
+      },
+      {
+        name: 'Joao',
+        img: './img/Joao.jpg'
+      },
+      {
+        name: 'Jorge',
+        img: './img/Jorge.jpg'
+      },
+      {
+        name: 'Marianna',
+        img: './img/Marianna.jpg'
+      },
+      {
+        name: 'Matheus',
+        img: './img/Matheus.jpg'
+      },
+      {
+        name: 'Murillo',
+        img: './img/Murillo.jpg'
+      },
+      {
+        name: 'Raphael',
+        img: './img/Raphael.jpg'
+      },
+      {
+        name: 'Karen',
+        img: ''
+      }
     ]);
-    this.students.forEach((student) => {
+    this.students.forEach(student => {
       this.addCard(student);
     });
 
     this.finalPairs = [];
 
-    $('.card').on('click', (e) => {
+    $('.card').on('click', e => {
       if (!$(e.currentTarget).hasClass('clicked')) {
         $(e.currentTarget).addClass('clicked');
         const name = $(e.currentTarget).attr('attr-name');
@@ -90,7 +87,7 @@ class Raffle {
       const chunks = _.chunk(this.finalPairs, 2);
       const pairs = $('#pairs');
       pairs.empty();
-      chunks.forEach((chunk) => {
+      chunks.forEach(chunk => {
         const pair = $(`
                 <div class="pair">
                 <span>${chunk[0]}</span>
